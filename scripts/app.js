59c3577c5d2847552892e8cc19c8c1cda1b2c79f
mgrinko <glukym@gmail.com>
2018-05-21 20:25:30 +0300

    phones catalogue was created

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import PhonesPage from './phones/phones-page.js';
+
+new PhonesPage({
+  element: document.querySelector('[data-page-container]')
+});
---
+++ scripts/phones/phones-page.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import PhonesCatalogue from './_components/phones-catalog.js';
+
+export default class PhonesPage {
+  constructor({ element }) {
+    this._element = element;
+
+    this._catalogue = new PhonesCatalogue({
+      element: this._element.querySelector('[data-component="phones-catalog"]'),
+      phones: [],
+    })
+  }
+}
