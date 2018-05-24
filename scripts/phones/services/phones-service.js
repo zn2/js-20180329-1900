2ced06c3f48efecf32c92b4bbe2eba7363103ce7
mgrinko <glukym@gmail.com>
2018-05-24 21:00:37 +0300

    sendRequest method was added

--- scripts/phones/services/phones-service.js
+++ scripts/phones/services/phones-service.js
@@ -3,23 +3,17 @@
 
 const PhonesService = {
   loadPhones(callback) {
-    let xhr = new XMLHttpRequest();
-
-    xhr.open('GET', '/api/phones', true);
-
-    xhr.send();
-
-    xhr.onload = () => {
-      let data = JSON.parse(xhr.responseText);
-
-      callback(data);
-    };
+    this._sendRequest('/api/phones', callback);
   },
 
   loadPhone(phoneId, callback) {
+    this._sendRequest(`/api/phones/${ phoneId }`, callback);
+  },
+
+  _sendRequest(url, callback, { method = 'GET' } = {}) {
     let xhr = new XMLHttpRequest();
 
-    xhr.open('GET', `/api/phones/${ phoneId }`, true);
+    xhr.open(method, url, true);
 
     xhr.send();
 
